85955691d90a71af20f07fb4649c9c479d162b19
beddinao <edbilal916@gmail.com>
2025-05-04 16:40:04 -0400

    setup documentation using Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	
+	const config = new DocumentBuilder()
+	.setTitle("The Metall Todo API")
+	.setDescription("an API with authentication in order to proper CRUDing")
+	.setVersion("1.001")
+	.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory)
+
+	await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
