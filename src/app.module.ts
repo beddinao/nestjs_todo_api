d0e0d4dd4db8a19bbb24ec433f705f94e85bf63f
beddinao <edbilal916@gmail.com>
2025-05-04 22:45:09 -0400

    wrap prisma into a usable service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [PrismaModule, AuthModule, UsersModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
