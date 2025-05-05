8091f4e58589d7a65a58b76bad199dc52e157f5e
beddinao <bilaleddinaoui@gmail.com>
2025-05-04 22:51:04 -0400

    main users services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private prisma: PrismaService) {}
+
+	async find_by_email(email: string) {
+		return this.prisma.user.findUnique({ where: { email } });
+	}
+
+	async find_by_id(id: number) {
+		return this.prisma.user.findUnique({ where: { id } });
+	}
+
+	async create_user(email: string, password: string) {
+		return this.prisma.user.create({
+			data: { email, password }
+		});
+	}
+}
