15b86aa7fc52b36e7addee14459ca822cf1ab864
beddinao <edbilal916@gmail.com>
2025-05-05 12:46:03 -0400

    add username to User model

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,7 +22,7 @@ export class AuthController {
 	}
 
 	@Post('register')
-	register(@Body() body: { email: string; password: string }) {
+	register(@Body() body: { email: string; password: string, username: string }) {
 		console.log("\nauth-controller: register");
 
 		if (body == undefined) throw new BadRequestException("invalid request body");
@@ -30,6 +30,8 @@ export class AuthController {
 		if (!body.email) throw new BadRequestException("email is Required");
 
 		if (!body.password) throw new BadRequestException("password is required");
+
+		if (!body.username) throw new BadRequestException("username is required");
 		
 		const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 		if (!email_regex.test(body.email)) throw new BadRequestException("invalid email format");
@@ -38,8 +40,12 @@ export class AuthController {
 
 		if (body.password.length < 6) throw new BadRequestException("password is too short: < 6");
 
-		if (body.password.length > 20) throw new BadRequestException("password is too long: > 20")
+		if (body.password.length > 20) throw new BadRequestException("password is too long: > 20");
 
-		return this.authService.register(body.email, body.password);
+		if (body.username.length < 3) throw new BadRequestException("username is too short: < 3");
+
+		if (body.username.length > 15) throw new BadRequestException("username is too long: > 15");
+
+		return this.authService.register(body.email, body.password, body.username);
 	}
 }
