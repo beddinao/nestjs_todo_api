a857bc60f5695085c37503afaae1aa32b35c9106
beddinao <edbilal916@gmail.com>
2025-05-05 17:54:33 -0400

    simple refresh-token logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,25 +19,21 @@ export class UsersController {
 
 	@Get()
 	get_users(@Request() req) {
-		console.log("Users-controller: get: get_users");
 		return this.usersService.get_users();
 	}
 
 	@Get(':id')
 	get_user(@Param('id') id: number, @Request() req) {
-		console.log("Users-controller: get(id): get_user: id: ", id);
 		return this.usersService.get_user(id);
 	}
 
 	@Put('update')
 	update_user(@Body() body: { username?: string; password?: string; email?: string }, @Request() req) {
-		console.log("Users-controller: put(id): update_user: id: ", req.user.userId);
 		return this.usersService.update_user(req.user.userId, body.username, body.password, body.email);
 	}
 
 	@Delete('delete')
 	delete_user(@Request() req) {
-		console.log("Users-controller: delete(id): delete_user: id: ", req.user.userId);
 		return this.usersService.delete_user(req.user.userId);
 	}
 }
