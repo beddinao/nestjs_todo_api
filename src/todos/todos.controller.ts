ba599fd8cd8d01dee6655215e56173f5454acbb6
beddinao <edbilal916@gmail.com>
2025-05-05 12:29:49 -0400

    debugin meterial

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -19,40 +19,40 @@ export class TodosController {
 
 	@Post()
 	createTodo(@Body() body: { title: string }, @Request() req) {
-		console.log("TodosController Post Create", body);
+		console.log("todos-controller: post: createTodo: ", body);
 		return this.todosService.create_todo(body.title, req.user.userId);
 	}
 
 	@Get()
 	getTodos(@Request() req) {
-		console.log("TodosController Get");
+		console.log("todos-controller: get: getTodos");
 		return this.todosService.get_todos(req.user.userId);
 	}
 
 	@Get(':id')
 	getTodo(@Param('id') id: string, @Request() req) {
-		console.log("TodosController Get id", id);
+		console.log("todos-controller: get(id): getTodo:", id);
 		return this.todosService.get_todo(id, req.user.userId);
 	}
 
 	@Put(':id')
 	updateTodo(@Param('id') id: string,
-		@Body() body: { title?: string; done?: boolean },
-		@Request() req)
-	{
-			console.log("TodosController Put update", id, body);
-			return this.todosService.update_todo(
-				id,
-				body.title,
-				body.done,
-				req.user.userId,
-			);
-	}
+		 @Body() body: { title?: string; done?: boolean },
+		 @Request() req)
+		 {
+			 console.log("todos-controller: put(id): updateTodo: ", id, body);
+			 return this.todosService.update_todo(
+				 id,
+				 body.title,
+				 body.done,
+				 req.user.userId,
+			 );
+		 }
 
-	@Delete(':id')
-	deleteTodo(@Param('id') id: string, @Request() req) {
-		console.log("TodosController delete id", id);
-		return this.todosService.delete_todo(id, req.user.userId);
-	}
+		 @Delete(':id')
+		 deleteTodo(@Param('id') id: string, @Request() req) {
+			 console.log("todos-controller: delete(id): deleteTodo: ", id);
+			 return this.todosService.delete_todo(id, req.user.userId);
+		 }
 
 }
